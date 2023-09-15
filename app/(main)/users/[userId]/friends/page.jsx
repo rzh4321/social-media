fba2ce6e2fd51d254aaf4b223ce060d45c045c6b
Ricky <rzh4321@gmail.com>
2023-09-15 14:58:59 -0400

    removed not-found pages and loading pages to test deployment

--- app/(main)/users/[userId]/friends/page.jsx
+++ app/(main)/users/[userId]/friends/page.jsx
@@ -1,15 +1,11 @@
 import User from "../../../../../models/User";
 import FriendsSection from "../../../../../components/FriendsSection";
 import "../../../../../styles/friends.css";
-import { notFound } from "next/navigation";
 
 async function findUser(userId) {
   const user = await User.findById(userId).populate(
     "friends friendRequestsSent friendRequestsReceived",
   );
-  if (!user) {
-    notFound();
-  }
   return user;
 }
 
